9c12e21a4712f0566c9b18362e8a56cbcc93b422
chris <grkcharge@gmail.com>
2019-07-11 15:33:15 -0700

    Patched pre-commit

--- pkgs/applications/version-management/git-and-tools/pre-commit/default.nix
+++ pkgs/applications/version-management/git-and-tools/pre-commit/default.nix
@@ -1,5 +1,8 @@
 { stdenv, python3Packages }:
-with python3Packages; buildPythonApplication rec {
+
+with python3Packages;
+
+buildPythonApplication rec {
   pname = "pre-commit";
   version = "1.17.0";
 
@@ -8,6 +11,10 @@ with python3Packages; buildPythonApplication rec {
     pname = "pre_commit";
     sha256 = "1qswk30w2cq8xvj16mhszsi3npp0z08s8lki1w67nif23c2kkk6c";
   };
+
+  patches = [
+    ./hardcoded-path-for-pre-commit-binary-in-hook.patch
+  ];
 
   propagatedBuildInputs = [
     aspy-yaml
@@ -24,6 +31,11 @@ with python3Packages; buildPythonApplication rec {
 
   # Tests fail due to a missing windll dependency
   doCheck = false;
+
+  preFixup = ''
+      substituteInPlace $out/lib/${python.libPrefix}/site-packages/pre_commit/resources/hook-tmpl \
+        --subst-var-by pre-commit $out
+  '';
 
   meta = with stdenv.lib; {
     description = "A framework for managing and maintaining multi-language pre-commit hooks";
